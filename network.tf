86f909f533ea4c16a1f9560627fa0e9df808d0a5
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2023-06-29 19:51:40 +0200

    Update network.tf
    changement ligne 3 adresse space = [local.address_space]

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = local.vnet_name
-  address_space       = local.address_space
+  address_space       = [local.address_space]
   location            = local.location
   resource_group_name = local.resource_group
 
