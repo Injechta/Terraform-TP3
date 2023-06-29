19d1e1628886de0d3b9298e0d689da5369888bcd
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2023-06-29 19:21:19 +0200

    Create locals.tf
    Création fichier locals.tf

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  vnet_name      = "my-vnet"
+  address_space  = ["10.1.0.0/16"]
+  location       = "westeurope"
+  resource_group = "Gregory_E"
+  subnet_count    = 2
+
+  tags = {
+    TP       = "terraform-azure-vnet-dynamic"
+    Owner    = "GrOoT"
+    DeployID = formatdate("DD-MM-YYYY", timestamp())
+  }
+}
