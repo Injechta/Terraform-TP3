79c2c89e2c5d76e35cb558aa178e7ca52f0885ad
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2023-06-29 19:38:39 +0200

    Create subnets.tf
    Ajout fichier subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,9 @@
+resource "azurerm_subnet" "subnets" {
+  count                = local.subnet_count
+  name                 = "subnet-${count.index}"
+  address_prefixes     = [cidrsubnet(local.address_space, 8, count.index)] #8 est le nombre de bits pour le sous réseau, equivalent au /24
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = local.resource_group
+
+  tags = local.tags
+}
